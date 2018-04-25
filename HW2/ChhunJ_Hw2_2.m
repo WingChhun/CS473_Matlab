ca4339687d9cf5272bf10fbd94a1613b1dd4513e
WingChhun <capt.wing.chhun@gmail.com>
2018-04-25 11:02:46 -0700

    Directory fomr hw 2

---
+++ HW2/ChhunJ_Hw2_2.m
@@ -0,0 +1,10 @@
+%James Chhun
+%Homework 2 Question 3
+
+%Write the code that generates a normal sample with given u and sigma, and
+% the code that calculates m and s fro the sample. Do the same using the
+% Bayes' estiator assuming a prior distribution for u.
+
+%Given
+mu = 2.0;
+sigma = 0.5;
